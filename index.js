38c723c9ccc489e53fcc698358a8ded24838b117
ARTHUR LINS DE OLIVEIRA CAVALCANTE <ARTHUR0699@digitalcollegesul.local>
2024-09-04 20:07:55 -0300

    Inicio dos metodos com array

---
+++ index.js
@@ -0,0 +1,25 @@
+//1- criar uma lista vazia
+let listaJogos = ['Lost planet', 'Crash 3', 'Ace Combate']
+
+let listaEstrela = [{nome:'Arthur', idade:22},'Beteguese', 'Sirius', 'Anilam', 12]
+
+//2- alterando o ultimo item para maiusculo
+
+// console.log(listaJogos[2].toUpperCase())
+
+//3- Alterar todos os elementos para maiúsculo
+
+//Map  cria um novo array a partir do array principal, forEach utiliza o mesmo array
+
+let pessoas = [
+    'AnGELo lusTOSA',
+    'Juan BenJaMin',
+    'MAtheus AraNHA'
+]
+
+
+// pessoas.forEach(item => console.log(item.toUpperCase()))
+
+
+//4- Adicionar item no array
+
